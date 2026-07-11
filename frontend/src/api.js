5fa5a3dbe24bca70ff05053395633e9f912d08f7
sai121663 <saithanpanchaharan123@gmail.com>
2026-07-11 16:07:22 -0400

    Updating Render backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@
 
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export const api = axios.create({
   baseURL: API_BASE,
